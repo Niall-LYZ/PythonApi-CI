4e8fdc4730e31e7a881739d963535de9cc48943f
Niall-LYZ <niall.lyz@gmail.com>
2025-08-14 12:06:13 +0100

    fixing issues with github actions - ran black reformat

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,10 +3,12 @@ from src.app import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert "Hello" in response.json()["message"]
+
 
 def test_health():
     response = client.get("/health")
