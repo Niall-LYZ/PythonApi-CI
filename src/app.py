6c29d98588fbc2fa94b86648e8cf6cf21986e6f5
Niall-LYZ <niall.lyz@gmail.com>
2025-08-14 11:40:54 +0100

    Refactor + adding in test code

--- src/app.py
+++ src/app.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "DevSecOps!"}
+def root():
+    return {"message": "Hello, DevSecOps!"}
 
 @app.get("/health")
-def read_health():
+def health():
     return {"status": "healthy"}
